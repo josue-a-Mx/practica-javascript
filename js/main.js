1553b7a0a7afa668fe09a95a73a0cf3ddd60a231
josue-a-Mx <josuearevalo55@gmail.com>
2019-10-10 13:06:07 -0500

    Prueba de Javascript. Primer ejercicio

---
+++ js/main.js
@@ -0,0 +1,26 @@
+
+/* let cantidadDeDolares = 700 //ECMAScript recomienda usar "let" en vez de "var"
+
+let tasaDeCambio = 21
+
+let cantidadPesosAAhorrar = cantidadDeDolares * tasaDeCambio
+
+console.log("La cantidad que Fulanito tiene que ahorrar es:", cantidadPesosAAhorrar, tasaDeCambio)
+ */
+/* let horaDeDormir = 1
+
+let horaDeDespertar = 5
+
+let horasDeDescanso = horaDeDespertar - horaDeDormir
+
+console.log("Dormiste por:", horasDeDescanso, "hora(s)") */
+
+let total = 1000
+
+let personas = 5
+
+let totalMasPropina = total + (total*0.10)
+
+let distribuirCuenta = totalMasPropina / personas
+
+console.log("A un restaurante asistieron", personas, "personas. Su consumo fue por un total de", totalMasPropina, "y cada persona aportó", distribuirCuenta, "pesos para cubrir el total, incluyendo propina de 10 por ciento.")
